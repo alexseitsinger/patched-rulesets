4d4514f9002968b4a3b9426790f68a78b8212955
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-12 13:47:18 -0400

    docs: Removes old comments.

--- src/plugins/node/patches.js
+++ src/plugins/node/patches.js
@@ -1,10 +1,1 @@
-// uses env: node, plugin: node
-
-/**
- * NOTE:
- *
- * This config should be listed BEFORE import, since import config patches
- * rules on this config.
- */
-
 module.exports = {}
