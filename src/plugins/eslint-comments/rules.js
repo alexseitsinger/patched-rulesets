ebf3b34f574e39c06235dddfb7ba266a492ac69d
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-12 13:46:27 -0400

    refactor: Allows whole file eslint disabled.

--- src/plugins/eslint-comments/rules.js
+++ src/plugins/eslint-comments/rules.js
@@ -1,12 +1,20 @@
 module.exports = {
-  "eslint-comments/disable-enable-pair": "error",
+  "eslint-comments/no-unlimited-disable": "error",
+  "eslint-comments/disable-enable-pair": [
+    "error",
+    {
+      // Allow disables for whole-files
+      allowWholeFile: true,
+    },
+  ],
   "eslint-comments/no-aggregating-enable": "error",
   "eslint-comments/no-duplicate-disable": "error",
-  "eslint-comments/no-unlimited-disable": "error",
   "eslint-comments/no-unused-disable": "error",
   "eslint-comments/no-unused-enable": "error",
-  //"eslint-comments/no-restricted-disable": "error"
-  //"eslint-comments/no-use": ["error", {
-  //  allow: [],
-  //}]
+  /*
+   * "eslint-comments/no-restricted-disable": "error"
+   * "eslint-comments/no-use": ["error", {
+   *   allow: [],
+   * }]
+   */
 }
