d431256ce5323706a3d22b68c59435cfffd8def0
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-12 13:48:26 -0400

    feat: Adds patch for eslint-comments.

--- src/plugins/unicorn/patches.js
+++ src/plugins/unicorn/patches.js
@@ -4,22 +4,17 @@ module.exports = {
      * Unicorn  has an enhanced version of this rule.
      */
     "no-nested-ternary": "off",
-
-    /**
-     * Unsure why this is here...
-     */
-    "prefer-destructuring": [
-      "error",
-      {
-        object: true,
-        array: false,
-      },
-    ],
   },
   "react-redux": {
     /**
      * We prefer to use 'unicorn/filename-case' over this rule.
      */
     "react-redux/connect-prefer-named-arguments": "off",
+  },
+  "eslint-comments": {
+    /**
+     * We prefer the uniron rule, so disabled this to prevent redundant errors.
+     */
+    "eslint-comments/no-unlimited-disable": "off",
   },
 }
