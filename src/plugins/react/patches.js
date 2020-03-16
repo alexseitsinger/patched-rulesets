dfe6f50086cbbca00ba04cbf257cecd1f46742d1
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-16 10:51:00 -0400

    feat: Adds patch to @typescript.

--- src/plugins/react/patches.js
+++ src/plugins/react/patches.js
@@ -1,3 +1,13 @@
-// react plugin
+module.exports = {
+  "@typescript-eslint": {
+    /**
+     * THis rule conflicts with jsx-wrap-multilines, so to prevent an infinite cycle of
+     * eslint errors, we just disable this rule completely
+     */
+    "@typescript-eslint/no-extra-parens": "off",
+  },
+  "eslint": {
+    ""
+  }
 
-module.exports = {}
+}
