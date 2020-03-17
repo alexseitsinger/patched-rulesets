f9e2d48d0c315b0d167c009c190302150d082e92
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-17 14:32:54 -0400

    test: Adds tests for setup.

---
+++ tests/setup.test.js
@@ -0,0 +1,102 @@
+const { createConfig } = require("../src/setup")
+const { getPlugins } = require("../src/groups")
+
+const patchedConfig = {
+  quotes: "single",
+  braceStyle: "stroustrup",
+  spaces: 4,
+  maxLength: 120,
+}
+
+describe("setup", () => {
+  it("should use the provided patched config and adjust rules appropriately", () => {
+    const config = createConfig({
+      pluginNames: getPlugins({
+        names: ["typescript"],
+        isES6: true,
+      }),
+      useEslint: true,
+      patchedConfig: patchedConfig,
+    })
+
+    const { rules } = config
+
+    expect(rules.quotes).toStrictEqual("off")
+    expect(rules["@typescript-eslint/quotes"]).toStrictEqual([
+      "error",
+      patchedConfig.quotes,
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      }
+    ])
+
+    expect(rules["max-len"]).toStrictEqual([
+      "error",
+      {
+        code: patchedConfig.maxLength,
+        tabWidth: patchedConfig.spaces,
+        comments: patchedConfig.maxLength,
+        ignorePattern: "^import\\W.*",
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ])
+
+    expect(rules.indent).toStrictEqual("off")
+    expect(rules["@typescript-eslint/indent"]).toStrictEqual([
+      "error",
+      patchedConfig.spaces,
+      {
+        SwitchCase: 1,
+        VariableDeclarator: {
+          var: 2,
+          let: 2,
+          const: 3,
+        },
+        outerIIFEBody: 1,
+        MemberExpression: 1,
+        FunctionDeclaration: {
+          parameters: "first",
+          body: 1,
+        },
+        FunctionExpression: {
+          parameters: "first",
+          body: 1,
+        },
+        CallExpression: {
+          arguments: "first",
+        },
+        ArrayExpression: 1,
+        ObjectExpression: 1,
+        ImportDeclaration: 1,
+        flatTernaryExpressions: true,
+        ignoredNodes: [],
+        ignoreComments: false,
+      },
+    ])
+  })
+  it("should use '@typescript-eslint/brace-style' and patch 'brace-style' off", () => {
+    const config = createConfig({
+      pluginNames: getPlugins({
+        names: ["typescript"],
+        isES6: true,
+      }),
+      useEslint: true,
+    })
+
+    const { rules } = config
+
+    expect(rules["brace-style"]).toStrictEqual("off")
+    expect(rules["@typescript-eslint/brace-style"]).toStrictEqual([
+      "error",
+      "stroustrup",
+      {
+        allowSingleLine: false,
+      },
+    ])
+  })
+})
