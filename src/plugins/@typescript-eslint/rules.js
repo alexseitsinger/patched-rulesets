f9047c9f3c1e1bbfce78054a7ef2c7483faeabb3
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-19 07:41:30 -0400

    refactor: Disables extra parens.

--- src/plugins/@typescript-eslint/rules.js
+++ src/plugins/@typescript-eslint/rules.js
@@ -482,7 +482,7 @@ module.exports = {
    *
    * https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-extra-parens.md
    */
-  "@typescript-eslint/no-extra-parens": "error",
+  "@typescript-eslint/no-extra-parens": "off",
 
   /**
    * Disallow unnecessary semicolons
