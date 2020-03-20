c8530928f96cce22339bb2bc3efe6f69891c2a0e
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-20 09:51:48 -0400

    feat: Adds patch for react/sort-comp.

--- src/plugins/sort-class-members/patches.js
+++ src/plugins/sort-class-members/patches.js
@@ -1,1 +1,9 @@
-module.exports = {}
+module.exports = {
+  react: {
+    /**
+     * This rule provides a more comprehensive rule for sorting class components, so
+     * just disable the react version.
+     */
+    "react/sort-comp": "off",
+  },
+}
