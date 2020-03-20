6eb28b30514c2930eca53dd378da146a80f6fe87
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-20 07:58:18 -0400

    refactor: Updates ignore pattern.
    This should prevent wrapping import/require statements.

--- src/plugins/eslint/rules.js
+++ src/plugins/eslint/rules.js
@@ -1,5 +1,13 @@
 const confusingBrowserGlobals = require("confusing-browser-globals")
 
+/**
+ * We specify some specific ignore patterns to exclude from our max-len rule.
+ * This should prevent our imports and require paths from wrapping to a second line.
+ */
+const requireStatement = `^(var|let|const)\\s.+\\s*=\\s*require\\(['"].+['"]\\)$`
+const importStatement = `^import\\s.+\\sfrom\\s['"].+['"]$`
+const maxLengthIgnorePattern = `(${requireStatement})|(${importStatement})`
+
 module.exports = {
   /**
    * Enforce linebreaks after opening and before closing array brackets.
@@ -505,12 +513,11 @@ module.exports = {
       code: 88,
       tabWidth: 2,
       comments: 88,
-
       /*
        * Setting an ignore pattern according to suggestion on github.
        * https://github.com/prettier/prettier/issues/1954
        */
-      ignorePattern: "^import\\W.*",
+      ignorePattern: maxLengthIgnorePattern,
       ignoreTrailingComments: true,
       ignoreUrls: true,
       ignoreStrings: true,
