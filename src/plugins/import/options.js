57b671d889f40a6d35caf6f9dd496cb498ff4d8b
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-22 12:39:44 -0400

    fix: Adds node builtins to core-modules.
    This prevents import from incorrectly reporting the use of them as
    imports.

--- src/plugins/import/options.js
+++ src/plugins/import/options.js
@@ -1,24 +1,73 @@
+const nodeBuiltins = [
+  "assert",
+  "buffer",
+  "child_process",
+  "cluster",
+  "console",
+  "constants",
+  "crypto",
+  "dgram",
+  "dns",
+  "domain",
+  "events",
+  "fs",
+  "https",
+  "module",
+  "net",
+  "os",
+  "path",
+  "punycode",
+  "querystring",
+  "readline",
+  "repl",
+  "stream",
+  "string_decoder",
+  "sys",
+  "timers",
+  "tls",
+  "tty",
+  "url",
+  "util",
+  "vm",
+  "zlib",
+  "freelist",
+  "v8",
+  "process",
+  "async_hooks",
+  "http2",
+  "perf_hooks",
+]
+
 module.exports = {
   env: {
     es6: true,
   },
   settings: {
+    /**
+     * If we end up using eslint_d or eslint-loader (long-lasting processes) we should
+     * adjust the lifetime value to something specific (in seconds) to automatically
+     * expire import cache.
+     */
+    "import/cache": {
+      lifetime: Infinity
+    },
     "import/internal-regex": "^packages/",
     "import/core-modules": [
       "electron",
+      /**
+       * If we don't specify these here, eslint will report errors when we try to use
+       * any builtins modules.
+       */
+      ...nodeBuiltins,
     ],
     "import/external-module-folders": [
       "node_modules",
       ".yalc",
-      "dist",
     ],
     "import/resolver": {
       node: {
-        extensions: [ ".js", ".jsx", ".ts", ".tsx", ".json" ]
+        extensions: [ ".js", ".jsx", ".json" ]
       },
-      jest: {
-        jestConfigFile: "./jest.config.js"
-      }
     }
   }
 }
